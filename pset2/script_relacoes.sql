5cba9be655eb885befea61535508c8c014bc18f4
Jarbas Santos <99821458+JarbaSantos@users.noreply.github.com>
2022-05-10 20:09:07 -0300

    Update script_relacoes.sql

--- pset2/script_relacoes.sql
+++ pset2/script_relacoes.sql
@@ -4,3 +4,33 @@ postgres
 psql
 computaco@raiz
 
+/*se necessario. (sempre que entro, preciso alterar para acessar o schema do elmasri) */
+alter user jarbas set search_path to elmasri, '\$user', public;
+
+\c uvv jarbas;
+99426929
+
+/* Questão 1 */
+SELECT   avg(salario)
+FROM     funcionario
+WHERE    numero_departamento=1
+UNION
+SELECT   avg(salario)                  
+FROM     funcionario
+WHERE    numero_departamento=4
+UNION
+SELECT   avg(salario)
+FROM     funcionario
+WHERE    numero_departamento=5;
+
+ /* Questão 2 */
+ SELECT  avg(salario)
+ FROM    funcionario
+ WHERE   sexo=M
+ UNION
+ SELECT  avg(salario)
+ FROM    funcionario
+ WHERE   sexo=F
+            
+ /* Questão 3 */
+ 
